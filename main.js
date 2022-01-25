d574b4d528dec1d1ad0019519c27280e016d3f4f
GeonwooShin <rjsdnslawkd@naver.com>
2022-01-25 20:07:10 +0900

    JS navbar

---
+++ main.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// 네비게이션바 스크롤하면 불투명하게끔
+const navbar = document.querySelector('#navbar')
+const navbarHeight = navbar.getBoundingClientRect().height
+
+document.addEventListener('scroll', () => {
+  if(window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark')
+  } else {
+    navbar.classList.remove('navbar--dark')
+  }
+})
+
+// 해당 탭으로 이동
+const navbarMenu = document.querySelector('.navbar__menu')
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target
+  const link = target.dataset.link
+  if(link === null) {
+    return 
+  }
+  console.log(event.target.dataset.link)
+  const scrollTo = document.querySelector(link)
+  scrollTo.scrollIntoView({behavior: "smooth"})
+})
